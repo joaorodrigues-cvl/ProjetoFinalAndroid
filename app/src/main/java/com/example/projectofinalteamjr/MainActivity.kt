2d962c4130475aa63dee17da83047b6b3ad41a1e
Joao Rodrigues <joaorodrigues.cvl@gmail.com>
2024-05-08 13:43:52 +0100

    commit codigo limpo final

--- app/src/main/java/com/example/projectofinalteamjr/MainActivity.kt
+++ app/src/main/java/com/example/projectofinalteamjr/MainActivity.kt
@@ -36,9 +36,9 @@ class MainActivity : AppCompatActivity() {
         // Rotina de criação de um curso novo
 
         val curso = Curso(
-            Nome = "Introduction to Programming Test3",
-            Descricao = "Learn the basics of programming Test3",
-            TotalHoras = 40
+            Nome = "Test3",
+            Descricao = "Test3",
+            TotalHoras = 50
         )
 
         val cursoAction = CursosActions() // Cria um novo objeto do tipo cursoAction
@@ -51,60 +51,4 @@ class MainActivity : AppCompatActivity() {
         cursoAction.getApiCursos() // chama a função desse novo objeto para buscar os cursos
 
     }
-   /* private fun sendRequestCursos(curso: Curso) {
-        val client = OkHttpClient.Builder().build()   // adicionado para funcionar..
-        val builder = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
-            .build()
-            .create(MyApi::class.java)
-        val call = builder.criarCurso(curso) // cria um novo curso do tipo curso de acordo com os dados anteriores
-
-        call.enqueue(object : Callback<Curso> {
-            override fun onResponse(call: Call<Curso>, response: Response<Curso>) {
-                // Handle successful response
-                if (response.isSuccessful) {
-                    val cursoCriado = response.body()
-                    Log.i(TAG2, "onResponse: Curso criado ${cursoCriado?.Nome}")
-                } else {
-                    Log.i(TAG2, "onResponse: Error ${response.message()}")
-                }
-            }
-
-            override fun onFailure(call: Call<Curso>, t: Throwable) {
-                // Handle failure
-                Log.i(TAG2, "onFailure: Network request failed: ${t.message}")
-            }
-        })
-    }
-
-
-    private fun getApiCursos() {
-        val api = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(MyApi::class.java)
-
-        api.getCursos().enqueue(object : Callback<List<Cursos>> {
-            override fun onResponse(
-                call: Call<List<Cursos>>,
-                response: Response<List<Cursos>>
-            ) {
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        for (curso in it) {
-                            Log.i(TAG, "onResponse: ${curso.Nome}")
-                        }
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<List<Cursos>>, t: Throwable) {
-                Log.i(TAG, "onFailure: ${t.message}")
-            }
-
-        })
-    }*/
 }
